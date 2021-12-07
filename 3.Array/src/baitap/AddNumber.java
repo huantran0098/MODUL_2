2a1cac3745ad3f1d7f6a60119b6d11c0d1ae5651
trhuan9800 <tr.huan.8098@gmail.com>
2021-12-07 16:26:08 +0700

    add baitap

--- 3.Array/src/baitap/AddNumber.java
+++ 3.Array/src/baitap/AddNumber.java
@@ -6,25 +6,24 @@ import java.util.Scanner;
 public class AddNumber {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10} ;
+        int[] array = new int[arr.length + 1] ;
         int number, index ;
         Scanner scanner = new Scanner(System.in) ;
         System.out.println("Enter position to add number.");
         index = scanner.nextInt();
         System.out.println("Enter number to add.");
         number = scanner.nextInt();
+        addElement(arr, array, number, index);
+    }
 
-        if (index <= -1 && index >= arr.length - 1){
-            System.out.println("Không chèn được đâu bạn ơi, té đi nhập lại!!");
-        }else {
-        for (int i = 0; i < index -1; i++){
-            arr[i] = arr[i];
+    public static void addElement ( int[] arr, int[] array , int number, int index ){
+        for (int i = 0; i < index -1; i++) {
+            array[i] = arr[i] ;
         }
-        for (int i = arr.length; i > index -1; i--) {
-            arr[i] = arr[i-1] ;
+        for (int i = arr.length; i > index - 1; i--) {
+            array[i] = arr[i - 1] ;
         }
-        arr[arr.length -1 ] = arr[arr.length -1];
-        arr[index -1] = number;
-            System.out.println("New array is: " + Arrays.toString(arr));
-        }
+        array[index - 1] = number ;
+        System.out.println("New array is: " + Arrays.toString(array));
     }
 }
--- 3.Array/src/baitap/FindMin.java
+++ 3.Array/src/baitap/FindMin.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class FindMin {
     public static void main(String[] args) {
-        int size ;
+        int row, col ;
+        int[][] arr ;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter size of array");
-        size = input.nextInt();
-        int[] arr = new int[size];
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++ {
+        System.out.println("Enter row of array");
+        row = input.nextInt();
+        System.out.println("Enter colum of array");
+        col = input.nextInt();
+        arr = new int[row][col];
+        int min = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++){
                 System.out.println("Enter element at row" + i + "colum " + j + ": ");
                 arr[i][j] = input.nextInt();
+
             }
         }
-        for (int i = 0; i < arr.length; i++ {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++){
                 if (arr[i][j] < min ){
-                    min = arr[i][j] ;
+                    min = arr[i][j];
                 }
             }
         }
